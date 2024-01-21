7301ec0bcd51e7e9e537f4f640d0b194ee1193f0
abolfazmz81 <abolfazmz81@gmail.com>
2024-01-21 15:28:13 +0330

    adding composite model to get location and user at the same time

---
+++ models/userLocationCompositeModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Server;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.SqlServer.Types;
+
+
+using hiBuddy.Models;
+
+namespace hiBuddy.models
+{
+    public class userLocationCompositeModel
+    {
+        public UserManagement User { get; set; }
+        public locations? Locations { get; set; }
+    }
+}
+
