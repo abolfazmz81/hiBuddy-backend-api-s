a425437a2991438e1151b0145c53e0f51d07ef58
abolfazmz81 <abolfazmz81@gmail.com>
2024-03-23 20:38:21 +0330

    fixup! feat: Add Register interfaces and base

---
+++ IAM/IAM.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using IAM.Application.AuthenticationService;
+using IAM.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAM.Presentation.Controllers;
+
+
+[ApiController]
+[Produces("application/json")]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IRegisterService _registerService;
+
+    public AuthenticationController(IRegisterService registerService)
+    {
+        _registerService = registerService;
+    }
+
+    [HttpPost("register")]
+    public ActionResult Register(User user)
+    {
+        var result = _registerService.Handle(user);
+        return Ok(result);
+    }
+
+    [HttpPut("TwoStep")]
+    public ActionResult TwoStep()
+    {
+        return Ok();
+    }
+}
