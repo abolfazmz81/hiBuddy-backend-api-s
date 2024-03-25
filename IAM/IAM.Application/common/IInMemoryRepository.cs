0838ce3fc8c239d9c650fe8deac5dd290f767323
abolfazmz81 <abolfazmz81@gmail.com>
2024-03-25 15:27:05 +0330

    feat: Add in-memory database interface and base

---
+++ IAM/IAM.Application/common/IInMemoryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace IAM.Application.common;
+
+public interface IInMemoryRepository
+{
+    void Add(String key);
+    String? Check(String key,String value);
+}
---
+++ IAM/IAM.Infrastructure/InMemoryRepository/InMemoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using IAM.Application.common;
+
+namespace IAM.Infrastructure.InMemoryRepository;
+
+public class InMemoryRepository : IInMemoryRepository
+{
+    private readonly String _db;
+    
+    public void Add(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string? Check(string key,String value)
+    {
+        throw new NotImplementedException();
+    }
+}
