ef0252280093fedfedfa3ccaee60d82fc8a4ba98
abolfazmz81 <abolfazmz81@gmail.com>
2024-03-25 17:42:37 +0330

    feat: Add more Dependency Injections

--- IAM/IAM.Application/DependencyInjection.cs
+++ IAM/IAM.Application/DependencyInjection.cs
@@ -8,6 +8,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IRegisterService, RegisterService>();
+        services.AddScoped<IAuthPhoneRegister, AuthPhoneRegister>();
         return services;
     }
 }
--- IAM/IAM.Infrastructure/DependencyInjection.cs
+++ IAM/IAM.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using IAM.Infrastructure.CodeGenerator;
+﻿using IAM.Application.common;
+using IAM.Infrastructure.CodeGenerator;
+using IAM.Infrastructure.InMemoryRepository;
+using IAM.Infrastructure.Logger;
 
 namespace IAM.Infrastructure;
 using IAM.Application.AuthenticationService;
@@ -9,6 +12,10 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddScoped<ICodeGenerator, RandomCodeGenerator>();
+        services.AddScoped<IInmemoryContext, RedisInMemoryContext>();
+        services.AddScoped<IInMemoryRepository, InMemoryRepository.InMemoryRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IMLogger, Logger.Logger>();
         return services;
     }
 }
