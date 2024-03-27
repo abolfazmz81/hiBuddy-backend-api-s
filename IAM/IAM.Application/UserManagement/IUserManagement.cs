a638196824557c7542cb1336a2bad030c79481e2
abolfazmz81 <abolfazmz81@gmail.com>
2024-03-27 19:34:21 +0330

    feat: Add UserManagement Interface and base

---
+++ IAM/IAM.Application/UserManagement/IUserManagement.cs
@@ -0,0 +1,9 @@
+﻿using IAM.Application.AuthenticationService;
+
+namespace IAM.Application.UserManagement;
+
+public interface IUserManagement
+{
+    Boolean DeleteUser(AuthResult details);
+    AuthResult? UpdateUser(AuthResult details);
+}
---
+++ IAM/IAM.Application/UserManagement/UserManagement.cs
@@ -0,0 +1,16 @@
+﻿using IAM.Application.AuthenticationService;
+
+namespace IAM.Application.UserManagement;
+
+public class UserManagement : IUserManagement
+{
+    public bool DeleteUser(AuthResult details)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AuthResult? UpdateUser(AuthResult details)
+    {
+        throw new NotImplementedException();
+    }
+}
