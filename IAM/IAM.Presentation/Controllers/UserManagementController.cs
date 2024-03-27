93f7df9f0ed8ba1226901a4f2a45b7128b44752e
abolfazmz81 <abolfazmz81@gmail.com>
2024-03-27 19:41:57 +0330

    feat: Create User management Controller

---
+++ IAM/IAM.Presentation/Controllers/UserManagementController.cs
@@ -0,0 +1,25 @@
+﻿using IAM.Application.AuthenticationService;
+using IAM.Application.UserManagement;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAM.Presentation.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("UserManagement")]
+public class UserManagementController : ControllerBase
+{
+    private readonly IUserManagement _userManagement;
+
+    public UserManagementController(IUserManagement userManagement)
+    {
+        _userManagement = userManagement;
+    }
+    
+    [HttpDelete("DelUser")]
+    public ActionResult DeleteUser(AuthResult details)
+    {
+        _userManagement.DeleteUser(details);
+        return Ok("user deleted successfully");
+    }
+}
