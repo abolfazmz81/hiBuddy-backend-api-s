3c255d67fd79e3dc9abf186b314f89fc9cdbed70
abolfazmz81 <abolfazmz81@gmail.com>
2024-03-27 14:58:18 +0330

    feat: Implement RegisterService.cs handle function

--- IAM/IAM.Application/AuthenticationService/RegisterService.cs
+++ IAM/IAM.Application/AuthenticationService/RegisterService.cs
@@ -1,22 +1,38 @@
-﻿using IAM.Domain;
+﻿using IAM.Application.common;
+using IAM.Domain;
 
 namespace IAM.Application.AuthenticationService;
 
 public class RegisterService : IRegisterService
 {
-    
+    private readonly IUserRepository _userRepository;
+
+    public RegisterService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
     public AuthResult? Handle(User user)
     {
         // check if user exists
-    
+        // check with username
+        if (_userRepository.GetByUsername(user.username) is not null)
+        {
+            throw new Exception("user with this username already exists");
+        }
+        // check with email
+        if (_userRepository.GetByEmail(user.email) is not null)
+        {
+                throw new Exception("user with this email address already exists");
+        }
         // create user
         
         // add user to database
-        
+        var newUser = _userRepository.Add(user);
         // create token
         String token = Guid.NewGuid().ToString();
         // return newly created user
-        return new AuthResult(user, token);
+        return new AuthResult(newUser, token);
     }
     
 }
