b67b9a7270a2e65ffed77252d06f3014968e8a43
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-07 14:02:39 +0330

    fix: Change SignUp needed details

--- IAM/IAM.Contracts/Authentication/SignupAllDetails.cs
+++ IAM/IAM.Contracts/Authentication/SignupAllDetails.cs
@@ -4,14 +4,7 @@ public record SignupAllDetails
 (
 String username,
 long phone_number,
-String? name,     
- String? password,
- String? job,
-String? education,      
-String? pic,     
-String? email,      
-String? hobbies,
-String? favorites,       
-String? Date,
-String? gender
+String name,     
+String password,
+String email     
     );
