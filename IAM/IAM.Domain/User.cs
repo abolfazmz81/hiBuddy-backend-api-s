e6b798edfe2966df9f58172d71d6d7f4f5a519f6
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-07 14:20:26 +0330

    feat: Add User build in constructor

--- IAM/IAM.Domain/User.cs
+++ IAM/IAM.Domain/User.cs
@@ -17,4 +17,24 @@ public class User
     public String? favorites { get; set; }        
     public String? Date { get; set; }
     public String? gender { get; set; }
+    public bool isverified { get; set; }
+
+    public static User Create(int id,String username,String name,String email, String password,long number)
+    {
+        return new User
+        {
+            user_id = id,
+            username = username,
+            name = name,
+            email = email,
+            password = password,
+            phone_number = number,
+            isverified = false
+        };
+    }
+
+    public void verify()
+    {
+        isverified = true;
+    } 
 }
