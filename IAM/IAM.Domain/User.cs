6de04a8d109f5c1267fc33cd273aa2cd056845b3
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-07 15:10:48 +0330

    fix: Fix Adduser malfunction

--- IAM/IAM.Domain/User.cs
+++ IAM/IAM.Domain/User.cs
@@ -3,7 +3,7 @@ namespace IAM.Domain;
 
 public class User
 {
-    
+    [Key]
     public int user_id { get; set; }
     public String username { get; set; }
     public long phone_number { get; set; }
