a5efc432814c299dadde85872547c8f833065bc3
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-22 11:57:59 +0330

    feat(Media): Add media controller for receiving files

---
+++ Media/Media.Presentation/Controllers/MediaController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Media.Presentation.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("Media")]
+public class MediaController: ControllerBase
+{
+    
+    [HttpPut("Save")]
+    public async Task<ActionResult> Save(IFormFile file)
+    {
+        //Stream stream = new FileStream(Path.Combine("../Media.Infrastructure/files",file.FileName), FileMode.Create);
+        //await file.CopyToAsync(stream);
+        
+        Console.WriteLine(file.FileName);
+        return Ok();
+    }
+}
