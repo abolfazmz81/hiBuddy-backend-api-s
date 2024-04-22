09ca7be400aa587ab245ae7dfa5402e1cfe8c072
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-23 00:17:46 +0330

    feat(Media): Add class for passing files

---
+++ Media/Media.Contracts/MediaFile.cs
@@ -0,0 +1,8 @@
+﻿namespace Media.Contracts;
+
+public class MediaFile
+{
+    public string FileName { get; set; }
+    public string ContentType { get; set; }
+    public Stream Content { get; set; }
+}
