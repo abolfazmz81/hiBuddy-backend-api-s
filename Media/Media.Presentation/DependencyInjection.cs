3833f3400a156da8cb849cdbfb2011b95573fcd2
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-24 14:48:16 +0330

    feat(Media): Add Jwt authentication Settings

--- Media/Media.Presentation/DependencyInjection.cs
+++ Media/Media.Presentation/DependencyInjection.cs
@@ -1,10 +1,40 @@
-﻿namespace Media.Presentation;
+﻿using System.Text;
+using Media.Infrastructure.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Media.Presentation;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        
+        services.AddAuth();
+        return services;
+    }
+    
+    private static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        var jwtSettings = new JwtSettings
+        {
+            Secret = "HiBuddy_users_top_secret_key_ggs",
+            Issuer = "http://localhost:5000",
+            Audience = "http://localhost:5000",
+            ExpiryMinutes = 60*24*15
+        };
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o =>
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                });
         return services;
     }
 }
