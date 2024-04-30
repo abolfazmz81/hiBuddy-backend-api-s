308a00b20a27e2daa4cbd80d1a4bb7b576f5a213
abolfazmz81 <abolfazmz81@gmail.com>
2024-04-30 18:24:51 +0330

    feat(Media): Inject MediaRepository in IMediaRepository

--- Media/Media.Infrastructure/DependencyInjection.cs
+++ Media/Media.Infrastructure/DependencyInjection.cs
@@ -8,7 +8,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddScoped<IJWTChecker, JWTChecker>();        
+        services.AddScoped<IJWTChecker, JWTChecker>();
+        services.AddScoped<IMediaRepository, MediaRepository.MediaRepository>();
         return services;
     }
 }
