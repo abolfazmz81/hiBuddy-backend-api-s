9fa576189e8fa6365f29613731e0ad1fddac80b4
abolfazmz81 <abolfazmz81@gmail.com>
2024-05-06 22:03:30 +0330

    feat(Media): Inject MongoRepository into IMongoRepository

--- Media/Media.Infrastructure/DependencyInjection.cs
+++ Media/Media.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Media.Application.Common;
 using Media.Infrastructure.JWT;
+using Media.Infrastructure.MediaRepository;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Media.Infrastructure;
@@ -9,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddScoped<IJWTChecker, JWTChecker>();
+        services.AddScoped<IMongoRepository, MongoRepository>();
         services.AddScoped<IMediaRepository, MediaRepository.MediaRepository>();
         return services;
     }
--- Media/Media.Infrastructure/MediaRepository/MediaRepository.cs
+++ Media/Media.Infrastructure/MediaRepository/MediaRepository.cs
@@ -1,12 +1,21 @@
 ﻿using Media.Application.Common;
 using Media.Contracts;
-
+using MongoDB.Driver;
+using MongoDB.Bson;
 namespace Media.Infrastructure.MediaRepository;
 
 public class MediaRepository : IMediaRepository
 {
+    private readonly IMongoRepository _mongoRepository;
+
+    public MediaRepository(IMongoRepository mongoRepository)
+    {
+        _mongoRepository = mongoRepository;
+    }
+
     public async Task<string> Add(MediaFile file, string username)
     {
-        throw new NotImplementedException();
+        
+        return "ok";
     }
 }
