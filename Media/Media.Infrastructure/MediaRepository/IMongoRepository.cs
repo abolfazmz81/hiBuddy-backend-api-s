81fb91992b390ace9e75c05d484a17b0d1c248b9
abolfazmz81 <abolfazmz81@gmail.com>
2024-05-06 22:02:41 +0330

    feat(Media): Add IMongoRepository

---
+++ Media/Media.Infrastructure/MediaRepository/IMongoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Media.Contracts;
+using MongoDB.Bson;
+
+namespace Media.Infrastructure.MediaRepository;
+
+public interface IMongoRepository
+{
+    
+    Task<BsonDocument> CreateDoc(MediaFile file, string username);
+}
---
+++ Media/Media.Infrastructure/MediaRepository/MongoRepository.cs
@@ -0,0 +1,25 @@
+﻿using Media.Contracts;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Media.Infrastructure.MediaRepository;
+
+public class MongoRepository : IMongoRepository
+{
+    private static MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+    private static IMongoDatabase database = client.GetDatabase("HiBuddy");
+    private static IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("");
+
+    public async Task<BsonDocument> CreateDoc(MediaFile file, string username)
+    {
+        BsonDocument document = new BsonDocument
+        {
+            {"_id",new BsonArray{"Row_id",2}},
+            {"User_name",username},
+            {"File_name",file.FileName},
+            {"Content_Type",file.ContentType},
+            {"Content",file.Content.ToString()},
+        };
+        return document;
+    }
+}
