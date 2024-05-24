4c3ef8c2291ea0dde6d5c81282d0597738a2ab6b
abolfazmz81 <abolfazmz81@gmail.com>
2024-05-24 17:27:27 +0330

    feat(Media): Implement GetMedia and inject it into IGetMedia

--- Media/Media.Application/DependencyInjection.cs
+++ Media/Media.Application/DependencyInjection.cs
@@ -7,6 +7,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ISaveMedia, SaveMedia>();
+        services.AddScoped<IGetMedia,GetMedia>();
         return services;
     }
 }
---
+++ Media/Media.Application/Media/GetMedia.cs
@@ -0,0 +1,37 @@
+﻿using Media.Application.Common;
+using Media.Contracts;
+using MongoDB.Bson;
+
+namespace Media.Application.Media;
+
+public class GetMedia : IGetMedia
+{
+    private readonly IMediaRepository _mediaRepository;
+    private readonly IJWTChecker _jwtChecker;
+
+    public GetMedia(IMediaRepository mediaRepository, IJWTChecker jwtChecker)
+    {
+        _mediaRepository = mediaRepository;
+        _jwtChecker = jwtChecker;
+    }
+
+    public async Task<MediaFile> GetFile(string token, string fileName)
+    {
+        // check if token is valid
+        String? user = _jwtChecker.get_Username(token);
+        if (user is null)
+        { 
+            return null;
+        }
+        // get the desired document
+        BsonDocument? file = await _mediaRepository.GetDoc(user, fileName);
+        if (file.IsBsonNull)
+        {
+            return null;
+        }
+        // create media File
+        MediaFile mediaFile =await _mediaRepository.CreateMedia(file);
+        // return media file
+        return mediaFile;
+    }
+}
