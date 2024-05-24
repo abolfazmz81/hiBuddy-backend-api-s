1f0d1c8ba739b1848487d896142b29cc9e953ea3
abolfazmz81 <abolfazmz81@gmail.com>
2024-05-24 17:30:21 +0330

    feat(Media): Add Function interfaces

--- Media/Media.Application/Common/IMediaRepository.cs
+++ Media/Media.Application/Common/IMediaRepository.cs
@@ -1,9 +1,14 @@
 ﻿using Media.Contracts;
 
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 namespace Media.Application.Common;
 
 public interface IMediaRepository
 {
     public Task<String> Add(Domain.Media media);
     public Task<int> GetLastId();
+    public Task<BsonDocument> GetDoc(String username, String fileName);
+    public Task<MediaFile> CreateMedia(BsonDocument file);
 }
