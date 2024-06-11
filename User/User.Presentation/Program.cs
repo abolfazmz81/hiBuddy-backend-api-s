da60155950ca896e81dccca7f4d28aff7f517f83
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-11 13:43:18 +0330

    feat(User): Add Cors and DependencyInjection functions

--- User/User.Presentation/Program.cs
+++ User/User.Presentation/Program.cs
@@ -1,11 +1,19 @@
+using User.Application;
+using User.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddApplication().AddPresentation();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>{
+    options.AddPolicy("AllowOrigin",builder => builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:5002").AllowAnyMethod().AllowAnyHeader());
+});
 
 var app = builder.Build();
 
