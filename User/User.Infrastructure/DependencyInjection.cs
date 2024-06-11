8ef27e3a207f0367c3a9287918b465f691ee3c77
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-11 15:08:26 +0330

    feat(User): Add Infrastructure Dependency Injection

---
+++ User/User.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using User.Application.common;
+using User.Infrastructure.Hasher;
+using User.Infrastructure.UserRepository;
+
+namespace User.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository.UserRepository>();
+        services.AddScoped<IHasher, SHA256Hasher>();
+        
+        services.AddDbContext<SQLServerContext>();
+        return services;
+    }
+}
--- User/User.Presentation/Program.cs
+++ User/User.Presentation/Program.cs
@@ -1,10 +1,11 @@
 using User.Application;
+using User.Infrastructure;
 using User.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddApplication().AddPresentation();
+builder.Services.AddApplication().AddPresentation().AddInfrastructure();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
