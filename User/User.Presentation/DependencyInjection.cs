181d683736b8848d72efee7173c7da95495ebbe4
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-11 13:42:13 +0330

    feat(User): Add Swagger and JWT Settings

---
+++ User/User.Presentation/DependencyInjection.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using User.Infrastructure.JWT;
+
+namespace User.Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddSwagger();
+        services.AddAuth();
+        return services;
+    }
+    private static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        var jwtSettings = new JwtSettings
+        {
+            Secret = "HiBuddy_users_top_secret_key_ggs",
+            Issuer = "http://localhost:5000",
+            Audience = "http://localhost:5000",
+            ExpiryMinutes = 60*24*15
+        };
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o =>
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                });
+        return services;
+    }
+
+    private static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Hibuddy",
+                Description = "An ASP.NET Core Web API for Hibuddy website",
+        
+            });
+    
+            options.AddSecurityDefinition("Bearer",new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "enter a valid JWT token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                }
+            );
+            
+            //var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+        return services;
+    }
+}
