89a461032ac402036c953b11e03376b3a86f4b22
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-11 13:42:31 +0330

    feat(User): Inject UserDelete in IUserDelete

---
+++ User/User.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using User.Application.UserManagement;
+
+namespace User.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IDeleteUser, DeleteUser>();
+        return services;
+    }
+}
