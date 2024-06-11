dc34834a064935918f91e0c0f0b4b70686c2a2e1
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-11 15:07:42 +0330

    feat(User): Implement User Repository

---
+++ User/User.Infrastructure/UserRepository/SQLServerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace User.Infrastructure.UserRepository;
+
+public class SQLServerContext : DbContext
+{
+    public SQLServerContext() {}
+    public SQLServerContext(DbContextOptions<SQLServerContext> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<Domain.User> Hibuddy_user { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"data source=localhost, 1434;initial catalog=HiBuddy;user id=sa;password=Scotflop1381;TrustServerCertificate=True");
+    }
+}
---
+++ User/User.Infrastructure/UserRepository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User.Application.common;
+
+namespace User.Infrastructure.UserRepository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly SQLServerContext _context;
+
+    public UserRepository(SQLServerContext context)
+    {
+        _context = context;
+    }
+    public async Task<Domain.User?> GetByUsername(string username)
+    {
+        return await _context.Hibuddy_user.SingleOrDefaultAsync(user => user.username == username);
+    }
+
+    public async Task DelUser(Domain.User user)
+    {
+        _context.Hibuddy_user.Remove(user);
+        await _context.SaveChangesAsync();    }
+}
