dffefc0f7546bc9a71673fee8d293b13c653934e
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-12 14:20:01 +0330

    feat(User): Add Info Class

---
+++ User/User.Contracts/Info.cs
@@ -0,0 +1,9 @@
+﻿namespace User.Contracts;
+
+public class Info
+{
+    public String job { get; set; }
+    public String education { get; set; }
+    public String hobbies { get; set; }
+    public String favorites { get; set; } 
+}
