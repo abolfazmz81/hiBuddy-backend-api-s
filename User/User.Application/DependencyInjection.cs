7398a893540116902c144e49285ef40a072b6bec
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-12 14:58:24 +0330

    feat(User): Inject AddInfo Class

--- User/User.Application/DependencyInjection.cs
+++ User/User.Application/DependencyInjection.cs
@@ -8,6 +8,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IDeleteUser, DeleteUser>();
+        services.AddScoped<IAddInfo, AddInfo>();
         return services;
     }
 }
