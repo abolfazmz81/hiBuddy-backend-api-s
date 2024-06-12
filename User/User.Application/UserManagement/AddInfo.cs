04e199018e4b5bce4d8ba572022eee3815436a29
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-12 14:57:36 +0330

    feat(User): Implement AddInfo Class

---
+++ User/User.Application/UserManagement/AddInfo.cs
@@ -0,0 +1,27 @@
+﻿using User.Application.common;
+using User.Contracts;
+
+namespace User.Application.UserManagement;
+
+public class AddInfo : IAddInfo
+{
+    private readonly IUserRepository _userRepository;
+
+    public AddInfo(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<string> addInfo(String username,Info info)
+    {
+        // get user by id 
+        Domain.User? user = await _userRepository.GetByUsername(username);
+        if (user is null)
+        {
+            return "not exits";
+        }
+        // update user
+        await _userRepository.UpdateInfo(user, info);
+        return "ok";
+    }
+}
