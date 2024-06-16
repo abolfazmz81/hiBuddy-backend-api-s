6bb232aa2ee5e756e07d9ad470ba201b8aeede50
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-16 15:38:09 +0330

    feat(User): Add Additional class

---
+++ User/User.Contracts/Additional.cs
@@ -0,0 +1,10 @@
+﻿namespace User.Contracts;
+
+public class Additional
+{
+    public String pic { get; set; }        
+    public String name { get; set; }        
+    public String username { get; set; }
+    public String Date { get; set; }
+    public String gender { get; set; }
+}
