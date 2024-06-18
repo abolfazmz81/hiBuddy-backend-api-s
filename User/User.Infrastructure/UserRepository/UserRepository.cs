ee0db628c4c597b12d8f02ae755c04ff622a8c4c
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-18 19:46:14 +0330

    feat(User): Implement GetNear function

--- User/User.Infrastructure/UserRepository/UserRepository.cs
+++ User/User.Infrastructure/UserRepository/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections;
+using Microsoft.EntityFrameworkCore;
 using User.Application.common;
 using User.Contracts;
 using User.Domain;
@@ -51,5 +52,26 @@ public class UserRepository : IUserRepository
         _context.Hibuddy_user.Update(user);
         await _context.SaveChangesAsync();
         return user;
+    }
+
+    public async Task<ArrayList> GetNear(Domain.User user)
+    {
+        var allusers = _context.user_locations.FromSqlRaw("with us(id,ax,ay) as (select Hibuddy_user.user_id,x,y from Hibuddy_user join user_locations on Hibuddy_user.user_id = user_locations.user_id join locations on user_locations.location_id = locations.location_id where Hibuddy_user.user_id = "+user.user_id+ ")\nselect * from user_locations as t where t.user_id in (select s.user_id from us, Hibuddy_user as s join user_locations on s.user_id = user_locations.user_id join locations on user_locations.location_id = locations.location_id where s.user_id != us.id and SQRT(POWER(x-ax,2) + POWER(y-ay,2)) <= 0.025)  \n");
+        //Console.WriteLine("here 2");
+        ArrayList all = new ArrayList();
+            
+        foreach (var x in allusers.ToList())
+        {
+            //Console.WriteLine(x.ToJson());
+            Domain.User a = await _context.Hibuddy_user.FindAsync(x.user_id);
+            locations b = await _context.locations.FindAsync(x.location_id);
+            userLocationCompositeModel c = new userLocationCompositeModel();
+            a.password = "";
+            c.User = a;
+            c.Locations = b;
+            all.Add(c);
+        }
+
+        return all;
     }
 }
